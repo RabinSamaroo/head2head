2d214360151886a8f6f8cb9d58d011422449499f
Rabin Samaroo <r1samaroo@ryerson.ca>
2025-07-22 20:17:10 -0400

    working with mobalytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       {/* Left iframe - First OP.GG instance */}
       <div className="w-1/2 h-full border-r border-gray-300">
         <iframe
-          src="/api/proxy?url=https://u.gg/lol/profile/na1/rabin-na1/overview"
+          src="https://mobalytics.gg/lol/profile/na/12th%20planet-na1/overview"
           className="w-full h-full border-0"
           title="OP.GG Player 1"
           sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-top-navigation"
@@ -14,7 +14,7 @@ export default function Home() {
       {/* Right iframe - Second OP.GG instance */}
       <div className="w-1/2 h-full">
         <iframe
-          src="/api/proxy?url=https://www.u.gg"
+          src="https://mobalytics.gg/lol/profile/na/rabin-na1/overview"
           className="w-full h-full border-0"
           title="OP.GG Player 2"
           sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-top-navigation"
